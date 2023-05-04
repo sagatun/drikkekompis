e16ac01a81e093fdc4779005c507e0f6ac13d3fa
Erik Sagatun <eriksagatun@gmail.com>
2023-05-04 10:36:53 +0200

    add some more category-synonyms

--- client/src/utils/categorySynonyms.ts
+++ client/src/utils/categorySynonyms.ts
@@ -1,9 +1,32 @@
 export const categorySynonyms: Record<string, string[]> = {
   brennevin: ["sprit"],
+  rosévin: ["rosevin"],
+  sider: ["cider"],
+  musserende_vin: [
+    "musserende",
+    "sjampis",
+    "sparkling",
+    "sprudlevann",
+    "bobler",
+    "sprudlende",
+    "sprudlende vin",
+    "sprudlevin",
+  ],
   // Legg til flere kategorier med synonymer her, hvis nødvendig
 };
 
 export const subCategorySynonyms: Record<string, string[]> = {
-  øl_india_pale_ale: ["IPA"],
+  alkoholfritt_alkoholfri_musserende_øvrig: [
+    "alkoholfri musserende",
+    "alkoholfritt musserende",
+  ],
+  musserende_vin_champagne_brut: ["champagne", "sjampis", "sjampanje"],
+  musserende_vin_champagne_extra_brut: ["champagne", "sjampis", "sjampanje"],
+  musserende_vin_champagne_rosé: ["champagne", "sjampis", "sjampanje"],
+  øl_india_pale_ale: ["IPA", "indian pale ale"],
+  øl_lys_lager: ["lager"],
+  øl_mørk_lager: ["lager"],
+  øl_porter___stout: ["porter", "stout"],
+  øl_red_amber: ["red ale", "amber"],
   // Legg til flere kategorier med synonymer her, hvis nødvendig
 };
